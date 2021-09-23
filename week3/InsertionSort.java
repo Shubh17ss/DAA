522c5e1337047a4abc0b6768795a3ad3510d2fc3
Shubh Sharma <74175165+Shubh17ss@users.noreply.github.com>
2021-09-23 21:15:57 +0530

    Update InsertionSort.java

--- week3/InsertionSort.java
+++ week3/InsertionSort.java
@@ -1,1 +1,32 @@
 
+import java.util.*;
+class insertion_2
+{
+    static void insertion(int []arr,int n)
+    {
+        for(int i=1;i<n;i++)
+        {
+            int index=i;
+            int val=arr[index];
+            while(index>0&&arr[index-1]>val)
+            {
+                arr[index]=arr[index-1];
+                index=index-1;
+            }
+            arr[index]=val;
+        }
+        for(int i=0;i<n;i++)
+        System.out.print(arr[i]+" ");
+    }
+    
+    public static void main(String args[])
+    {
+        Scanner Sc=new Scanner(System.in);
+        int n;
+        n=Sc.nextInt();
+        int arr[]=new int[n];
+        for(int i=0;i<n;i++)
+        arr[i]=Sc.nextInt();
+        insertion(arr,n);
+    }
+}
